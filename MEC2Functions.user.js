27f62912b218a5f623a640df0972beb13b2b1c8d
MECH2-at-Github <150382173+MECH2-at-Github@users.noreply.github.com>
2024-02-15 11:20:12 -0600

    Update MEC2Functions.user.js

--- MEC2Functions.user.js
+++ MEC2Functions.user.js
@@ -4,7 +4,7 @@
 // @description  Add functionality to MEC2 to improve navigation and workflow
 // @author       MECH2
 // @match        mec2.childcare.dhs.state.mn.us/*
-// @version      0.3.3
+// @version      0.3.4
 // ==/UserScript==
 /* globals jQuery, $, waitForKeyElements */
 
